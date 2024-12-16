e95c58eee7ca5959cc3774f161c27cfa23277dac
Th. Nouskalis <athanasiosnouskalis@gmail.com>
2024-12-16 21:44:50 +0200

    project created

---
+++ src/gr/aueb/cf/ch1/IntroduceYourself.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.ch1;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class IntroduceYourself {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Thanos Nouskalis");
+    }
+}
